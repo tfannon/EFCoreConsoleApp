97333d5c69b8a53897e6000242dc4caeec6eeca8
tfannon <tfannon@gmail.com>
2017-01-02 10:52:06 -0500

    efcoreconsoleapp

---
+++ src/EFCoreConsoleApp/Migrations/BloggingContextModelSnapshot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EFCoreConsoleApp;
+
+namespace EFCoreConsoleApp.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    partial class BloggingContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EFCoreConsoleApp.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("EFCoreConsoleApp.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("EFCoreConsoleApp.Post", b =>
+                {
+                    b.HasOne("EFCoreConsoleApp.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- src/EFCoreConsoleApp/Program.cs
+++ src/EFCoreConsoleApp/Program.cs
@@ -9,6 +9,25 @@ namespace EFCoreConsoleApp
     {
         public static void Main(string[] args)
         {
+            using (var db = new BloggingContext())
+            {
+                if (!db.Blogs.Any())
+                {
+                    Console.WriteLine("Creating some blog entries");
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var semiRandomName = Guid.NewGuid().ToString().Substring(0, 8);
+                        var blog = new Blog {Url = $"http://{semiRandomName}.com"};
+                        db.Blogs.Add(blog);
+                    }
+                    db.SaveChanges();
+                }
+                Console.WriteLine("Reading blog entries");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($"{blog.BlogId} : {blog.Url}");
+                }
+            }
         }
     }
 }
